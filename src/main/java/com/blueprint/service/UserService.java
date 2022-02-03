265e02accffffda836d939d9855e9b0458c674a1
oren <orenitzc@gmail.com>
2022-02-03 13:27:28 +0200

    update

--- src/main/java/com/blueprint/service/UserService.java
+++ src/main/java/com/blueprint/service/UserService.java
@@ -3,24 +3,43 @@ package com.blueprint.service;
 import com.blueprint.model.User;
 import com.blueprint.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
 
-    private static UserRepository repository;
+    private final UserRepository repository;
 
-    public UserService(UserRepository repository) {
-        this.repository = repository;
-    }
-
-    public static User newUser(User user){
+    public User newUser(User user){
         User user2 = repository.save(user);
         return user2;
     }
-    public static Optional<User> getUserById(long id){
+    public Optional<User> getUserById(long id){
         Optional<User> user = repository.findById(id);
         return user;
+    }
+
+    @Transactional(timeout = 10)
+    public void updateSamePhoneForAllUsers(int phoneNumber) {
+        List<User> users = repository.findAll();
+        for(User user : users) {
+            user.setPhoneNumber(phoneNumber);
+            repository.save(user);
+        }
+        try {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException ex) {}
+        System.out.println("exit updateSamePhoneForAllUsers");
+        //throw new RuntimeException("stam");
+    }
+
+    @PostConstruct
+    public void post() {
+        System.out.println("Post Construct");
     }
 }
